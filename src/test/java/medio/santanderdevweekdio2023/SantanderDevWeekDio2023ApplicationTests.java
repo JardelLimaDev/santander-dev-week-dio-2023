9754718825a72064d18316b3eeaade6268ae0252
JardelLimaDev <jardeldelimagomes@gmail.com>
2023-11-12 20:06:14 -0300

    Initial commit

---
+++ src/test/java/medio/santanderdevweekdio2023/SantanderDevWeekDio2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package medio.santanderdevweekdio2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWeekDio2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
